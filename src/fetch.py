3e8063b13a615fc15c8405867416a369bb0e2c4f
Will Trimble <trimble@anl.gov>
2013-05-14 15:35:01 -0500

    removed diagnostic dump of header

--- src/fetch.py
+++ src/fetch.py
@@ -50,11 +50,13 @@ def retrieveMGRbyaccession(accession, rformat="fasta"):
 #  http://api.metagenomics.anl.gov/reads/mgm4447971.3   OBSOLETE
     a = re.search("^(4......\..)$", accession).group(1)
     if key == "":
-        sys.stderr.write("MGR webkey not defined\n")
-        s1 = "curl http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/ -D /tmp/fetch-dump > %s.gz"  % ( a, a ) 
+        sys.stderr.write("Warning: MGR webkey not defined\n")
+#        s1 = "curl http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/ -D /tmp/fetch-dump > %s.gz"  % ( a, a ) 
+        s1 = "curl http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/                     > %s.gz"  % ( a, a ) 
     else: 
         sys.stderr.write("Using MGR webkey %s\n" % key)
-        s1 = "curl 'http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/?&auth=%s' -D /tmp/fetch-dump > %s.gz" % ( a, key, a )
+#        s1 = "curl 'http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/?&auth=%s' -D /tmp/fetch-dump > %s.gz" % ( a, key, a )
+        s1 = "curl 'http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/?&auth=%s'  > %s.gz" % ( a, key, a )
 #    jsonobject = os.popen(s).read()
 #    print jsonobject
     sys.stderr.write("Executing %s\n" % s1) 
@@ -81,7 +83,7 @@ def retrieveSRRbyaccession(accession, rformat="fastq"):
     os.system(s)
   
 if __name__ == '__main__':
-    usage  = "usage: %prog <accession number> [-f <format>]"
+    usage  = "usage: fetch.py <accession number> [-f <format>]"
     parser = OptionParser(usage)
     parser.add_option("-f", "--format", dest="rformat", default=None, help="Data format (fasta, fastq, gbk) ")
     (opts, args) = parser.parse_args()
