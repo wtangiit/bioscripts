ebb3a5e7868a092a8d7ffde057570d137ff47ca9
Will Trimble <trimble@anl.gov>
2013-05-14 11:20:39 -0500

    cleanup

--- src/fetch.py
+++ src/fetch.py
@@ -8,8 +8,8 @@
 
 #ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByStudy/sra/SRP/SRP009/SRP009660 
 
-#ftp://ftp.sra.ebi.ac.uk/vol1/ERA007/ERA007448/  # example
-#ftp://ftp.sra.ebi.ac.uk/vol1/SRR387/SRR387449   #fails
+#ftp://ftp.sra.ebi.ac.uk/vol1/ERA007/ERA007448/srf   # example
+#ftp://ftp.sra.ebi.ac.uk/vol1/SRR387/SRR387449   #  fails
 #ftp://ftp.sra.ebi.ac.uk/vol1/SRA048/SRA048196   #fails
 
 #ftp://ftp.sra.ebi.ac.uk/vol1/srr/SRR387/SRR387449  # seems to return gz
@@ -23,12 +23,14 @@ from optparse import OptionParser
 
 def retrieveGENBANKbyaccession(accession, rformat="fasta"):
     from Bio import Entrez
+    if rformat == None:
+        rformat = "fasta"
     sys.stderr.write("Downloading %s, requested format %s\n" % (accession, rformat))
     Entrez.email = "trimble@anl.gov"
     handle = Entrez.efetch(db="nucleotide", id=accession, rettype=rformat)
     if rformat == "fasta":
         f = open("%s.fna" % accession, "w")
-    elif rformat == "gb" or rformat=="gbk" or rformat=="genbank":
+    elif rformat == "gb" or rformat == "gbk" or rformat == "genbank":
         f = open("%s.gbk" % accession, "w")
     elif rformat == "ft":
         f = open("%s.ft" % accession, "w")
@@ -39,21 +41,19 @@ def retrieveGENBANKbyaccession(accession, rformat="fasta"):
     f.close()
 
 def retrieveWGSbyaccession(accession, rformat="fasta"):
-    print "four-digit accession %s"%accession
+    print "four-digit accession %s" % accession
     s = "curl ftp://ftp.ncbi.nih.gov/genbank/wgs/wgs.%s.1.fsa_nt.gz >  %s.fna.gz" % (accession, accession)
     os.system(s) 
 
 def retrieveMGRbyaccession(accession, rformat="fasta"):
 #  http://api.metagenomics.anl.gov/sequences/mgm4447971.3 OBSOLETE
 #  http://api.metagenomics.anl.gov/reads/mgm4447971.3   OBSOLETE
-    a=re.search("^(4......\..)$", accession).group(1)
-    if key=="":
+    a = re.search("^(4......\..)$", accession).group(1)
+    if key == "":
         sys.stderr.write("MGR webkey not defined\n")
-        s = "curl http://api.metagenomics.anl.gov/sequenceset/mgm%s/ -D /tmp/fetch-dump"  % ( a, ) 
         s1 = "curl http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/ -D /tmp/fetch-dump > %s.gz"  % ( a, a ) 
     else: 
         sys.stderr.write("Using MGR webkey %s\n" % key)
-        s = "curl 'http://api.metagenomics.anl.gov/sequenceset/mgm%s/?&auth=%s' -D /tmp/fetch-dump" % ( a, key )
         s1 = "curl 'http://api.metagenomics.anl.gov/sequenceset/mgm%s-050-1/?&auth=%s' -D /tmp/fetch-dump > %s.gz" % ( a, key, a )
 #    jsonobject = os.popen(s).read()
 #    print jsonobject
@@ -65,36 +65,35 @@ def retrieveSRRbyaccession(accession, rformat="fastq"):
         asperapath=os.environ["ASPERAPATH"]
     except KeyError:
         sys.exit("Error: environment variable ASPERAPATH must be defined and point to aspera to use SRR download")
-    a= re.search(".RR(......)", accession).group(1)
-    print  "Fetching SRR accession %s"%accession
+    print  "Fetching SRR accession %s" % accession
     tla      = accession[0:3]
     stem     = accession[0:6]
     filename = accession+".sra"
     s = "%s/bin/ascp -l 300m -QT -i %s/etc/asperaweb_id_dsa.putty anonftp\@ftp-trace.ncbi.nlm.nih.gov:sra/sra-instant/reads/ByRun/sra/%s/%s/%s/%s ./%s" % ( asperapath, asperapath, tla, stem, accession, filename, filename)
-    print "with %s"%(s)
+    print "with %s" % (s)
     if int(os.popen("ascp 2>&1 |wc").read().split()[0]) > 10:
-      os.system(s)
+        os.system(s)
     else: 
-      sys.exit("Sorry, can't find ascp.\t")
+        sys.exit("Sorry, can't find ascp.\t")
     print s 
-    s = "fastq-dump --split-3 ./%s & "%(filename)
+    s = "fastq-dump --split-3 ./%s & " % (filename)
     print s
     os.system(s)
   
 if __name__ == '__main__':
     usage  = "usage: %prog <accession number> [-f <format>]"
     parser = OptionParser(usage)
-    parser.add_option("-f", "--format", dest="rformat", default=None, help="Data format")
+    parser.add_option("-f", "--format", dest="rformat", default=None, help="Data format (fasta, fastq, gbk) ")
     (opts, args) = parser.parse_args()
     try:
-        key=os.environ["MGRKEY"]
+        key = os.environ["MGRKEY"]
     except KeyError:
-        key=""
+        key = ""
   
     try :
         accession = args[0]
     except IndexError:
-        parser.error.exit("accession is a required parameter\n%s"%usage)
+        parser.error("accession is a required parameter\n%s" % usage)
   
     if re.search("^(4......\..)$", accession):  # MGR accession
         retrieveMGRbyaccession(accession)
@@ -113,5 +112,5 @@ if __name__ == '__main__':
     elif re.search("^(.U\d\d\d\d\d\d)$", accession):  #  Provisional REFSEQ
         retrieveGENBANKbyaccession(accession, rformat=opts.rformat)
     else: 
-        print "Don't recognize acecssion %s"%accession
-    	sys.exit()
+        print "Don't recognize acecssion %s" % accession
+        sys.exit()
